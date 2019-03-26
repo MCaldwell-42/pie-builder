68174468140ca0167bd3c5ab9d96d87d4a2a1d90
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-03-25 20:31:34 -0500

    ticket2

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('moooooooo')
+const addButton = document.getElementById('addButton');
+const inputIngredient = document.getElementById('inputIngredient');
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += textToPrint;
+};
+
+const addIngredient = (e) => {
+    e.preventDefault();
+    const inputText = inputIngredient.value;
+    printToDom('ingredient-container', inputText);
+    inputIngredient.value = '';
+};
+
+const eventListeners = () => {
+    
+    addButton.addEventListener('click', addIngredient);
+};
+
+const init = () => {
+    eventListeners();
+};
+
+init();
